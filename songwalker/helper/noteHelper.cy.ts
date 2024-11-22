6f9da891bd10667c62122d7ebd9b8dcd143de322
Ari Asulin <ari.asulin@phoenix.edu>
2024-11-21 23:52:50 -0500

    working on notehelper parseNote

--- songwalker/helper/noteHelper.cy.ts
+++ songwalker/helper/noteHelper.cy.ts
@@ -1,8 +1,28 @@
-import {parseFrequencyString, parseNote} from "@songwalker/helper/noteHelper";
+import {parseNote} from "@songwalker/helper/noteHelper";
 
 describe('noteHelper', () => {
-    it('parseNote', async () => {
+    it('parseNote C#4v0.5d1/2', async () => {
         const parts = parseNote('C#4v0.5d1/2');
-        expect(parts).to.eq({})
+        expect(parts).to.deep.eq({
+            "note": "C#",
+            "octave": 4,
+            "frequency": 272.1429467772926,
+            "params": {
+                "velocity": 0.5,
+                "duration": 0.5
+            }
+        })
+    })
+    it('parseNote C#4v.5d/2', async () => {
+        const parts = parseNote('C#q4v.5d/2');
+        expect(parts).to.deep.eq({
+            "note": "C#q",
+            "octave": 4,
+            "frequency": 280.1173438046181,
+            "params": {
+                "velocity": 0.5,
+                "duration": 0.5
+            }
+        })
     })
 })
--- songwalker/helper/noteHelper.ts
+++ songwalker/helper/noteHelper.ts
@@ -1,28 +1,26 @@
 const DEFAULT_FREQUENCY_A4 = 432;
 
-const REGEX_COMMAND = /^([A-G][#qb]{0,2})(\d)?((?:[vd]\d(?:[./]\d)?)+)$/
-const REGEX_COMMAND_PARAMS = /([vd])((\d)(?:([.\/])(\d))?)/g
+const REGEX_COMMAND = /^([A-G][#qb]{0,2})(\d*)((?:[vd]\d*(?:[.\/]\d+)?)+)$/
+const REGEX_COMMAND_PARAMS = /([vd])((\d*)(?:([.\/])(\d+))?)/g
 
 interface ParsedNoteCommandParams {
     velocity?: number,
     duration?: number
 }
 interface ParsedNoteCommand {
+    note: string,
+    octave: number,
+    frequency: number,
     params: ParsedNoteCommandParams
 }
 
-export function parseNote(noteString: string):ParsedNoteCommand|null {
-    const match = noteString.match(REGEX_COMMAND);
-    if(!match)
-        return null;
-    const [, note, octaveString, paramString] = match;
+export function parseNoteParams(paramString: string) {
     const params:ParsedNoteCommandParams = {}
-    if(paramString) {
         const paramMatches = [...paramString.matchAll(REGEX_COMMAND_PARAMS)];
         for(const paramMatch of paramMatches) {
             let [, paramName, paramValue, paramNumerator, valueFraction, valueDenominator] = paramMatch;
             let calcValue = (valueFraction === '/')
-            ? parseInt(paramNumerator) / parseInt(valueDenominator)
+                ? (paramNumerator ? parseInt(paramNumerator) : 1) / parseInt(valueDenominator)
                 : parseFloat(paramValue)
             switch(paramName) {
                 case 'v':
@@ -32,11 +30,30 @@ export function parseNote(noteString: string):ParsedNoteCommand|null {
                     params.duration = calcValue
                     break;
             }
-            debugger;
-
         }
-    }
+        return params;
+}
+
+export function parseNote(noteString: string):ParsedNoteCommand|null {
+    const match = noteString.match(REGEX_COMMAND);
+    if(!match)
+        return null;
+    const [, note, octaveString, paramString] = match;
+    const octave: number = parseInt(octaveString);
+    if (typeof LIST_NOTE_NAMES[note] === "undefined")
+        throw new Error("Unrecognized Note: " + note);
+    let keyNumber: number = LIST_NOTE_NAMES[note];
+    if (keyNumber < 6)
+        keyNumber = keyNumber + 24 + ((octave - 1) * 24) + 2;
+    else
+        keyNumber = keyNumber + ((octave - 1) * 24) + 2;
+    let frequency = DEFAULT_FREQUENCY_A4 * Math.pow(2, (keyNumber - 98) / 24);
+    const params = parseNoteParams(paramString);
     return {
+        note,
+        octave,
+        // keyNumber,
+        frequency,
         params
     }
 }
